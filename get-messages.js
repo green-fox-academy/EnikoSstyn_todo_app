885a34cc3bdfffc4dba1fd263a270a28c2a58c23
EnikoSstyn <35663980+EnikoSstyn@users.noreply.github.com>
2020-12-14 16:09:20 +0100

    instruction message to the todoApp

---
+++ get-messages.js
@@ -0,0 +1,7 @@
+function instructionMessage(){
+    console.log('Parancssori Todo applikáció\n ============================= \n\n Parancssori argumentumok:\n -l   Kilistázza a feladatokat\n -a   Új feladatot ad hozzá\n -r   Eltávolít egy feladatot\n -c   Teljesít egy feladatot');
+
+}
+
+
+export {instructionMessage};
